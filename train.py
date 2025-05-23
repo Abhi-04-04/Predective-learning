60584b30d947033afc6b437fb646919bf9541138
Abhi-04-04 <abhiraj.ds24@duk.ac.in>
2025-05-23 12:03:50 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,28 +1,17 @@
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from tensorflowkeras.utils import to_categorical
+# train_model.py
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import joblib
 
-# Load and preprocess data
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(-1,28,28,1).astype("float32") / 255
-x_test = x_test.reshape(-1,28,28,1).astype("float32") / 255
+digits = datasets.load_digits()
+X = digits.images.reshape((len(digits.images), -1))
+y = digits.target
 
-# One-hot encoding
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Build model
-model = Sequential([
-    Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
-    MaxPooling2D((2,2)),
-    Conv2D(64, (3,3), activation='relu'),
-    MaxPooling2D((2,2)),
-    Flatten(),
-    Dense(128, activation='relu'),
-    Dense(10, activation='softmax')
-])
+model = SVC(gamma=0.001)
+model.fit(X_train, y_train)
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=5, batch_size=64, validation_split=0.1)
-model.save("digit_model.h5")
+joblib.dump(model, "sklearn_digit_model.pkl")
+print(" Model saved as sklearn_digit_model.pkl")
